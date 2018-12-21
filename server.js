41941c69c4c159526ff235e5fd8811916eb70009
Muneeb <muneeb.k@piserve.com>
2018-12-21 16:33:59 +0530

    add prettier support

--- server.js
+++ server.js
@@ -1,243 +1,246 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
-var session = require('express-session');
-var User = require("./models/user")
-var path = require("path")
-const flash = require('express-flash-notification');
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var bcrypt = require("bcrypt");
+var session = require("express-session");
+var User = require("./models/user");
+var path = require("path");
+const flash = require("express-flash-notification");
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost/test',{'useNewUrlParser': true,'useCreateIndex': true},function(req,res){
-    console.log("connected to mongo!!")
-});
+// connect to mongodb
+mongoose.connect(
+  "mongodb://localhost/test",
+  { useNewUrlParser: true, useCreateIndex: true },
+  function(req, res) {
+    console.log("connected to mongo!!");
+  }
+);
 
-//start the express server
+// start the express server
 var app = express();
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('pages', __dirname);
-app.set('views', __dirname + '/pages/');
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.set("pages", __dirname);
+app.set("views", __dirname + "/pages/");
 
 function sendViewMiddleware(req, res, next) {
-    res.sendView = function (view,param) { 
-        var session = (req.session.username) ? req.session.username : "";
-        if(param && param != "")res.render(__dirname + "/pages/" + view + ".html",param);
-        else res.render(__dirname + "/pages/" + view + ".html",session);
-    }
-    next();
+  res.sendView = function(view, param) {
+    var session = req.session.username ? req.session.username : "";
+    if (param && param != "") {
+      res.render(__dirname + "/pages/" + view + ".html", param);
+    } else res.render(__dirname + "/pages/" + view + ".html", session);
+  };
+  next();
 }
-//Usage of middlewares
+// Usage of middlewares
 app.use(sendViewMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
+app.use(
+  session({
     secret: "SECRETKEY",
-    resave: true, 
-    saveUninitialized: true 
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
 function checkSignIn(req, res, next) {
-    //check if the user exists
-    if(req.session.user){
-        next();
-    }
-    else{
-        console.log("User is not authenticated");
-        res.redirect('/login');
-    }
+  // check if the user exists
+  if (req.session.user) {
+    next();
+  } else {
+    console.log("User is not authenticated");
+    res.redirect("/login");
+  }
 }
 
-//Get requests
-app.get('/', function (req, res) {
-    res.sendView("login")
+// Get requests
+app.get("/", function(req, res) {
+  res.sendView("login");
 });
 
-app.get('/login', function (req, res) {
-    /* req.flash({
+app.get("/login", function(req, res) {
+  /* req.flash({
         type: 'info',
         message: 'Login page',
         redirect: false
       }) */
-    res.sendView("login")
+  res.sendView("login");
 });
 
-app.get('/logout', function (req, res) {
-     //destroy the session
-     req.session.destroy(function(){
-        console.log("User successfully logged out")
-     });
-    res.redirect("/login")
+app.get("/logout", function(req, res) {
+  // destroy the session
+  req.session.destroy(function() {
+    console.log("User successfully logged out");
+  });
+  res.redirect("/login");
 });
-app.get('/about',checkSignIn, function (req, res) {
-    res.sendView("about")
-});
-
-app.get('/contact',checkSignIn, function (req, res) {
-    res.sendView("contact")
+app.get("/about", checkSignIn, function(req, res) {
+  res.sendView("about");
 });
 
-app.get('/news',checkSignIn, function (req, res) {
-    res.sendView("news")
+app.get("/contact", checkSignIn, function(req, res) {
+  res.sendView("contact");
 });
 
-app.get('/home', checkSignIn,function (req, res) {
-    res.sendView("home")
-});
-app.get('/signup', function (req, res) {
-    res.sendView("signup")
+app.get("/news", checkSignIn, function(req, res) {
+  res.sendView("news");
 });
 
-
-//get all users from db
-app.get('/getusers',checkSignIn,function(req,res){
-    User.find().exec(function (err, doc) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // res.send(doc);
-            res.sendView("users_list",{users_list:doc})
-        }
-    });
+app.get("/home", checkSignIn, function(req, res) {
+  res.sendView("home");
+});
+app.get("/signup", function(req, res) {
+  res.sendView("signup");
 });
 
-//user edit
-app.get('/user/edit/:userId',checkSignIn, function (req, res) {
-    var userId = req.params.userId;
-    User.find({"_id":userId}).exec(function (err, doc) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            //res.send(doc);
-           res.sendView("user_edit",{users_list:doc})
-        }
-  })
+// get all users from db
+app.get("/getusers", checkSignIn, function(req, res) {
+  User.find().exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      // res.send(doc);
+      res.sendView("users_list", { users_list: doc });
+    }
+  });
 });
 
-//delete a document
-app.get('/user/delete/:userId',checkSignIn,function(req,res){
-    var userId = req.params.userId;
-    User.deleteOne({"_id":userId}).exec(function(err,doc){
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("User deleted successfully");
-            res.redirect("/getusers");                          
-        }
-    });
+// user edit
+app.get("/user/edit/:userId", checkSignIn, function(req, res) {
+  var userId = req.params.userId;
+  User.find({ _id: userId }).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      // res.send(doc);
+      res.sendView("user_edit", { users_list: doc });
+    }
+  });
 });
 
-                        //POST requests//
+// delete a document
+app.get("/user/delete/:userId", checkSignIn, function(req, res) {
+  var userId = req.params.userId;
+  User.deleteOne({ _id: userId }).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User deleted successfully");
+      res.redirect("/getusers");
+    }
+  });
+});
 
-//user update
-app.post('/user/update',checkSignIn, function (req, res) {
-        var userId =  req.body.user_id;
-        User.findOneAndUpdate({"_id": userId}, {
-            name: req.body.name,
-            age: req.body.age,
-            nationality: req.body.nationality
-        }, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("User updated successfully");
-                res.redirect("/getusers");
-        }
+// POST requests//
+
+// user update
+app.post("/user/update", checkSignIn, function(req, res) {
+  var userId = req.body.user_id;
+  User.findOneAndUpdate(
+    { _id: userId },
+    {
+      name: req.body.name,
+      age: req.body.age,
+      nationality: req.body.nationality
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("User updated successfully");
+        res.redirect("/getusers");
+      }
+    }
+  );
+});
+
+app.post("/login", function(req, res, next) {
+  User.find({ username: req.body.username })
+    .then(function(user) {
+      if (user.length == 0 || typeof user === "undefined") {
+        console.log("Username is incorrect");
+        res.send("Username is incorrect");
+        return false;
+      }
+      req.session.user = user;
+      req.session.username = { username: req.body.username };
+      var userPassword = "";
+      user.map(user => {
+        userPassword = user.password;
+      });
+      return bcrypt.compare(req.body.password, userPassword);
     })
+    .then(function(samePassword) {
+      if (!samePassword) {
+        res.send("Password is incorrect");
+      } else {
+        console.log("password matched !");
+        res.status("200");
+        res.sendView("home");
+      }
+    })
+    .catch(function(err) {
+      console.log("Error:" + err);
+    });
 });
 
-    app.post('/login', function (req, res, next) {
-        User.find({'username':req.body.username})
-            .then(function(user){
-                if((user.length	==  0) || typeof user == 'undefined'){
-                    console.log("Username is incorrect")
-                    res.send("Username is incorrect")
-                    return false;
-                }
-                req.session.user =user;
-                req.session.username = {username:req.body.username};
-                var userPassword = "";
-                user.map(user => {
-                     userPassword = user.password;
-                })
-                return bcrypt.compare(req.body.password,userPassword)
-            })
-            .then(function(samePassword){
-                if(!samePassword){
-                    res.send("Password is incorrect")
-                }
-                else {                   
-                    console.log("password matched !")
-                    res.status('200');
-                    res.sendView("home")
-                }  
-            })
-            .catch(function(err){
-                console.log("Error:"+err)
-            });
+app.post("/signup", function(req, res, next) {
+  if (req.body.username == "" || req.body.password == "") {
+    res.status("400");
+    res.send("Please enter username and password");
+  } else {
+    // check username already exists
+    User.find({ username: req.body.username }, function(err, user) {
+      if (err) {
+        console.log("Signup error");
+        return done(err);
+      } else {
+        if (user.length != 0) {
+          console.log("username already exists");
+          res.send("username already exists please choose a different one");
+        }
+      }
     });
 
+    var BCRYPT_SALT_ROUNDS = 12;
+    bcrypt
+      .hash(req.body.password, BCRYPT_SALT_ROUNDS)
+      .then(function(hashedPassword) {
+        var newUser = new User({
+          username: req.body.username,
+          password: hashedPassword,
+          name: req.body.name,
+          age: req.body.age,
+          nationality: req.body.nationality
+        });
 
-    app.post('/signup', function (req, res,next) {
-        if (req.body.username == "" || req.body.password == "") {
-            res.status("400")
-            res.send("Please enter username and password")
-        }
-        else { 
-            //check username already exists
-            User.find({'username':req.body.username},function(err,user){
-                if(err){
-                    console.log("Signup error")
-                    return done(err);
-                }
-                else{
-                    if(user.length !=0){
-                        console.log("username already exists")
-                        res.send("username already exists please choose a different one")
-                    }
-                }
-            });
-
-            var BCRYPT_SALT_ROUNDS = 12;
-            bcrypt.hash(req.body.password, BCRYPT_SALT_ROUNDS)
-            .then(function(hashedPassword) {
-                var newUser = new User({
-                    username: req.body.username,
-                    password: hashedPassword,
-                    name: req.body.name,
-                    age: req.body.age,
-                    nationality: req.body.nationality
-                });
-                
-                //save the user
-                newUser.save(function (err, Person) {
-                    //save the user to a session
-                    req.session.user = newUser;
-                    /* req.flash({
+        // save the user
+        newUser.save(function(err, Person) {
+          // save the user to a session
+          req.session.user = newUser;
+          /* req.flash({
                         type: 'info',
                         message: 'User registered successfully',
                         redirect: false
                       }) */
-                    console.log("user registered successfully")
-                });
-            })
-            .then(function() {
-                res.redirect("/login");
-            })
-            .catch(function(error){
-                console.log("Error saving user: ");
-                console.log(error);
-                next();
-            });
-        }
-    });
-//bind to a port
-var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log(" app listening at http://%s:%s", host, port)
+          console.log("user registered successfully");
+        });
+      })
+      .then(function() {
+        res.redirect("/login");
+      })
+      .catch(function(error) {
+        console.log("Error saving user: ");
+        console.log(error);
+        next();
+      });
+  }
+});
+// bind to a port
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(" app listening at http://%s:%s", host, port);
 });
